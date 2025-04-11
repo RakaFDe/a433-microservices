cb695d7552b7171d9b85c600ad45e11cc2e16a55
RakaFDe <rakafde@gmail.com>
2025-04-11 18:34:13 +0700

    ubah port 80

--- getlogcompose.sh
+++ getlogcompose.sh
@@ -1,2 +1,2 @@
-#docker-compose logs --tail=500 > docker_log_compose.txt
-docker-compose logs  > docker_log_compose.txt
+#dockercompose logs --tail=500 > docker_log_compose.txt
+docker compose logs  > docker_log_compose.txt
